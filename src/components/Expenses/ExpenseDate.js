6b13e4d1e8896e730784c1aefa2c7174dbfb0396
Damian Puś <35109367+DragHem@users.noreply.github.com>
2022-05-27 13:53:41 +0200

    1. React Basic - Components

---
+++ src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,17 @@
+import "./ExpenseDate.css";
+
+const ExpenseDate = ({ date }) => {
+  const month = date.toLocaleString("pl-PL", { month: "long" }).toUpperCase();
+  const day = date.toLocaleDateString("pl-PL", { day: "2-digit" });
+  const year = date.getFullYear();
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__year">{year}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
